203de1aea97a82d3fdaef6666dd35c98534e59aa
Michael Sarkisian <msarkisian1996@gmail.com>
2022-09-14 15:32:10 -0400

    create user deck frontend

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -3,18 +3,36 @@ import { useContext } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../userContext';
 import { DraftCard } from './DraftCard';
 
 export const Create = () => {
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
   const [draft, setDraft] = useState([]);
+  const [sumbitError, setSubmitError] = useState(false);
   const [user] = useContext(UserContext);
   const cardKey = useRef(0);
+  const navigate = useNavigate();
   useEffect(() => {
     if (window.localStorage.getItem('deckDraft'))
-      setDraft(window.localStorage.getItem('deckDraft'));
+      setDraft(JSON.parse(window.localStorage.getItem('deckDraft')));
+    if (window.localStorage.getItem('titleDraft'))
+      setTitle(window.localStorage.getItem('titleDraft'));
+    if (window.localStorage.getItem('descDraft'))
+      setDesc(window.localStorage.getItem('descDraft'));
   }, []);
+  useEffect(() => {
+    window.localStorage.setItem('deckDraft', JSON.stringify(draft));
+  }, [draft]);
+  useEffect(() => {
+    window.localStorage.setItem('titleDraft', title);
+  }, [title]);
+  useEffect(() => {
+    window.localStorage.setItem('descDraft', desc);
+  }, [desc]);
 
   const handleChange = (id, side, content) => {
     const idx = draft.findIndex((card) => card.id === id);
@@ -30,6 +48,31 @@ export const Create = () => {
     setDraft([...draft.slice(0, idx), ...draft.slice(idx + 1)]);
   };
 
+  const submitDeck = () => {
+    if (!title || draft.some((card) => card.front === '' || card.back === '')) {
+      setSubmitError(true);
+      setTimeout(() => setSubmitError(false), 4000);
+    } else {
+      const body = {
+        name: title,
+        cards: draft.map((card) => {
+          return { front: card.front, back: card.back };
+        }),
+      };
+      if (desc) body.description = desc;
+      fetch('/userdecks', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      }).then((res) => {
+        if (res.status === 200) {
+          window.localStorage.clear();
+          navigate('/privatedecks');
+        }
+      });
+    }
+  };
+
   if (!user)
     return (
       <div>
@@ -39,6 +82,8 @@ export const Create = () => {
   return (
     <>
       <h1>Deck builder</h1>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input value={desc} onChange={(e) => setDesc(e.target.value)} />
       <div>
         {draft.length} card{draft.length !== 1 && <>s</>} in deck
         <button
@@ -60,6 +105,12 @@ export const Create = () => {
           />
         );
       })}
+      {draft.length > 0 && <button onClick={submitDeck}>Create deck</button>}
+      {sumbitError && (
+        <div>
+          Deck requires a title. and all cards must have a front and a back.
+        </div>
+      )}
     </>
   );
 };
